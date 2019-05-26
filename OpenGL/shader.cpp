1da1a4aba191cdd7f2d6cbc7374168be06b6c973
赵伟 <atalia.zhaowei@gmail.com>
2019-05-26 23:02:22 +0800

    #shader

---
+++ OpenGL/shader.cpp
@@ -0,0 +1,49 @@
+﻿#include<shader.h>
+Shader::Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
+{
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+
+	vShaderFile.exceptions(std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::badbit);
+	try
+	{
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
+		std::stringstream vShaderStream, fShaderStream;
+
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+
+		vShaderFile.close();
+		fShaderFile.close();
+
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch(std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+	}
+	const GLchar* vShaderCode = vertexCode.c_str();
+	const GLchar* fShaderCode = fragmentCode.c_str();
+
+	GLuint vertex, fragment;
+	GLint success;
+	GLchar infoLog[512];
+
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vertexCode, NULL);
+	glCompileShader(vertex);
+
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+
+}
+void Shader::Use()
+{
+
+}
