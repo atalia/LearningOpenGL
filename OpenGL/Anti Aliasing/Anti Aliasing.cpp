62aed57b3ca528c3e12f7f7ebde8ff6359364da3
atalia <atalia.zhaowei@gmail.com>
2019-07-28 20:03:48 +0800

    Anti Aliasing

---
+++ OpenGL/Anti Aliasing/Anti Aliasing.cpp
@@ -0,0 +1,243 @@
+#pragma comment(lib, "openGL32.lib")
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include "Camera.h"
+#include "shader.h"
+#include <string>
+
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+const int VERSION_MINOR = 3;
+const int VERSION_MAJOR = 3;
+const int NR_POINT_LIGHTS = 4;
+
+
+bool keys[1024];
+
+bool firstMouse = true;
+GLfloat lastX = 400;
+GLfloat lastY = 300;
+Camera camera(glm::vec3(0.0f, 0.0f, 4.0f));//全局唯一的相机
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	if (key > 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			keys[key] = false;
+		}
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double posX, double posY)
+{
+	//std::cout << posX << "," <<posY << std::endl;
+	if (firstMouse)
+	{
+		lastX = posX;
+		lastY = posY;
+		firstMouse = false;
+	}
+	GLfloat xoffset = posX - lastX;
+	GLfloat yoffset = posY - lastY;
+	lastX = posX;
+	lastY = posY;
+	camera.processMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
+{
+	camera.processMouseScroll(yoffset);
+}
+
+void move(Camera& camera, GLfloat& deltaTime)
+{
+	if (keys[GLFW_KEY_W])
+		camera.processKeyboard(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.processKeyboard(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.processKeyboard(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.processKeyboard(RIGHT, deltaTime);
+}
+
+
+
+void matrixShow(glm::mat4& matrix)
+{
+	std::cout << std::string(10, '-') << std::endl;
+	for (int i = 0; i < matrix.length(); ++i)
+	{
+		for (int j = 0; j < matrix[0].length(); ++j)
+		{
+			std::cout << matrix[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::string(10, '-') << std::endl;
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, VERSION_MINOR);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, VERSION_MAJOR);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Anti Aliasing", NULL, NULL);
+	if (window == NULL)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetScrollCallback(window, scroll_callback);
+	glewExperimental = GL_TRUE;
+	
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW INIT FAILURE" << std::endl;
+		return -1;
+	}
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	glEnable(GL_DEPTH_TEST);
+	
+	Shader shader("./Anti Aliasing/shader.vert", "./Anti Aliasing/shader.frag");
+
+
+	GLfloat cubeVertices[] = {
+		// Positions       
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f
+	};
+
+	GLuint vao, vbo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	
+	
+	std::cout << vao << std::endl;
+	std::cout << vbo << std::endl;
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)(0));
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	
+	/*
+	GLuint ubo;
+	glGenBuffers(1, &ubo);
+	glBindBuffer(GL_UNIFORM_BUFFER, ubo);
+	glBufferData(GL_UNIFORM_BUFFER, 2 * sizeof(glm::mat4), NULL, GL_STATIC_DRAW);
+	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+	
+	shader.Use();
+	GLuint bindingpoint = 0;
+	glUniformBlockBinding(shader.Program, glGetUniformBlockIndex(shader.Program, "Matrice"), bindingpoint);
+	glBindBufferBase(GL_UNIFORM_BUFFER, bindingpoint, ubo);
+	*/
+
+	//Content End
+	GLfloat lastTime = 0.0f;
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+		GLfloat currentTime = glfwGetTime();
+		GLfloat deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+		move(camera, deltaTime); 
+
+		glm::mat4 projection = glm::perspective(camera.getZoom(), (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.1f, 1000.0f);
+		glm::mat4 view = camera.getView();
+		glm::mat4 model(1.0f);
+
+		shader.Use();
+		/*
+		glBindBuffer(GL_UNIFORM_BUFFER, ubo);
+		glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(projection));
+		glBufferSubData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), sizeof(glm::mat4), glm::value_ptr(view));
+		glBindBuffer(GL_UNIFORM_BUFFER, 0);
+		*/
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "project"), 1, GL_FALSE, glm::value_ptr(projection));
+
+
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+		
+		glfwSwapBuffers(window);
+		
+	}
+	glfwTerminate();
+	return 0;
+}
